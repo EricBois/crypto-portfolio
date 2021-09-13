c7d5c0fcd45603fbaa6ce007bcbdd745bc6573ad
burn4live <ericb@linux.com>
2021-09-13 15:58:59 -0600

    fixed bug and added footer

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,7 +18,7 @@ export default function Portfolio(props) {
     const formik = useFormik({
         initialValues: {
             ticker: '',
-            holdings: '0'
+            holdings: ''
         },
         validationSchema: validationTickerSchema,
         onSubmit: (coin, { resetForm }) => {
@@ -29,11 +29,17 @@ export default function Portfolio(props) {
 
     async function deleteTicker(tick) {
         let arr = watch
-        const index = arr.findIndex(x => x.ticker === tick)
-        if (index > -1) { arr.splice(index, 1) }
-        await setWatch(arr)
-        await fetchData()
-        await saveToDb()
+        if (watch.length === 1) {
+            await setWatch([])
+            await saveToDb()
+            await setCoins([])
+        } else {
+            const index = arr.findIndex(x => x.ticker === tick)
+            if (index > -1) { arr.splice(index, 1) }
+            await setWatch(arr)
+            await fetchData()
+            await saveToDb()
+        }
     }
 
     function saveToDb() {
@@ -47,23 +53,25 @@ export default function Portfolio(props) {
 
     // fetch data
     async function fetchData() {
-        // get all coin names
-        const coins = watch.map(coin => coin.ticker)
-        await axios
-            .get("https://api.nomics.com/v1/currencies/ticker?key=" + process.env.REACT_APP_NOMICS_API_KEY + "&ids=" + coins)
-            .then(resp => {
-                setCoins(resp.data)
-                // if coin isnt returned by the api , remove from watch list and db
-                if (resp.data.length !== coins.length) {
-                    let newArr = watch
-                    newArr.pop()
-                    setWatch(newArr)
-                    saveToDb()
-                }
-            })
-            .catch(err => {
-                console.log("Error fetching data from nomics", err);
-            });
+        if (watch.length > 0) {
+            // get all coin names
+            const coins = watch.map(coin => coin.ticker)
+            await axios
+                .get("https://api.nomics.com/v1/currencies/ticker?key=" + process.env.REACT_APP_NOMICS_API_KEY + "&ids=" + coins)
+                .then(resp => {
+                    setCoins(resp.data)
+                    // if coin isnt returned by the api , remove from watch list and db
+                    if (resp.data.length !== coins.length) {
+                        let newArr = watch
+                        newArr.pop()
+                        setWatch(newArr)
+                        saveToDb()
+                    }
+                })
+                .catch(err => {
+                    console.log("Error fetching data from nomics", err);
+                });
+        }
     }
 
     async function getInitialData() {
@@ -77,60 +85,66 @@ export default function Portfolio(props) {
     }
 
     useEffect(() => {
+        getInitialData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
         if (watch && watch.length > 0) {
             fetchData()
             saveToDb()
-        } else {
-            getInitialData()
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [watch])
 
     return (
-        <Grid container alignItems="center" justifyContent="center" spacing={1}>
-            <Grid item xs={12}>
-                <form onSubmit={formik.handleSubmit}>
-                    <Grid container justifyContent="center">
-                        <Grid item xs={10} sm={6} md={4} lg={3}>
-                            <TextField
-                                value={formik.values.holdings}
-                                onChange={formik.handleChange}
-                                margin="normal"
-                                label="Holdings"
-                                name="holdings"
-                                fullWidth
-                                variant="outlined"
-                                error={formik.touched.holdings && Boolean(formik.errors.holdings)}
-                                helperText={formik.touched.holdings && formik.errors.holdings}
-                            />
-                            <TextField
-                                value={formik.values.ticker}
-                                onChange={formik.handleChange}
-                                margin="normal"
-                                label="Ticker  BTC / ADA / ETH ..."
-                                name="ticker"
-                                fullWidth
-                                variant="outlined"
-                                error={formik.touched.ticker && Boolean(formik.errors.ticker)}
-                                helperText={formik.touched.ticker && formik.errors.ticker}
-                            />
-                            <Button
-                                color="primary"
-                                variant="contained"
-                                fullWidth
-                                type="submit"
-                            >
-                                ADD
-                            </Button>
+        <div style={{ minHeight: '90vh' }}>
+            <Grid container alignItems="center" justifyContent="center" spacing={1} >
+                <Grid item xs={12}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <Grid container justifyContent="center">
+                            <Grid item xs={10} sm={6} md={4} lg={3}>
+                                <TextField
+                                    value={formik.values.holdings}
+                                    onChange={formik.handleChange}
+                                    margin="normal"
+                                    label="Holdings"
+                                    type="number"
+                                    name="holdings"
+                                    fullWidth
+                                    variant="outlined"
+                                    error={formik.touched.holdings && Boolean(formik.errors.holdings)}
+                                    helperText={formik.touched.holdings && formik.errors.holdings}
+                                />
+                                <TextField
+                                    value={formik.values.ticker}
+                                    onChange={formik.handleChange}
+                                    margin="normal"
+                                    label="Ticker  BTC / ADA / ETH ..."
+                                    name="ticker"
+                                    fullWidth
+                                    variant="outlined"
+                                    error={formik.touched.ticker && Boolean(formik.errors.ticker)}
+                                    helperText={formik.touched.ticker && formik.errors.ticker}
+                                />
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    fullWidth
+                                    type="submit"
+                                >
+                                    ADD
+                                </Button>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form>
-            </Grid>
-            {coins.map((coin) => (
-                <Grid item xs={12} md={8} key={coin.id}>
-                    <Ticker coin={coin} holdings={watch} deleteTicker={() => deleteTicker(coin.id)} />
+                    </form>
                 </Grid>
-            ))}
-        </Grid>
+                {coins.map((coin) => (
+                    <Grid item xs={12} md={8} key={coin.id}>
+                        <Ticker coin={coin} holdings={watch} deleteTicker={() => deleteTicker(coin.id)} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
     )
 }
