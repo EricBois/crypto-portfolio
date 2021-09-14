6a868db2e2bacb3253e303c84a5d87e580addbd1
burn4live <ericb@linux.com>
2021-09-13 19:40:26 -0600

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            <footer style={{textAlign: 'center'}}><a href="https://nomics.com" target="_blank" rel="noreferrer">Crypto Market Cap & Pricing Data Provided By Nomics</a></footer>
+        <div style={{marginTop:'.5vh'}}>
+            <footer style={{textAlign: 'center', fontSize: '.7em'}}><a href="https://nomics.com" target="_blank" rel="noreferrer">Crypto Market Cap & Pricing Data Provided By Nomics</a></footer>
         </div>
     )
 }
